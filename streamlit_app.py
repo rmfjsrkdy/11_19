065e56b322558f24f16be48796b07e99de4900fb
rmfjsrkdy <injin032@gmail.com>
2025-11-19 15:10:55 +0000

    end

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,19 @@ from openai import OpenAI
 st.title("GPT")
 
 api_key = st.text_input("API Key", type="password")
-st.session_state.api_key = api_key
 
-client = OpenAI(api_key=api_key)
+if api_key:
+    st.session_state["OPENAI_API_KEY"] = api_key
+    client = OpenAI(api_key=api_key)
+    st.session_state["openai_client"] = client
+else:
+    st.write("API 키를 입력하세요")
 
 user_question = st.text_area("Prompt")
 if st.button("Submit"):
-    response = client.chat.completions.create(
+    response = response = client.responses.create(
         model="gpt-5-mini",
-        messages=[
-            {"role": "user", "content": user_question}
-            ]
-            )
-    answer = response.choices[0].message.content
+        input=user_question
+    )
+    answer = response.output_text
     st.write(answer)
